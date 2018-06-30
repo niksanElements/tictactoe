9d238492e7002a516c9157d84d88da14ce067ebf
niksanElements <niksan9411@gmail.com>
2018-06-30 22:40:23 +0300

    stop the update hadler when ever it should be stopped

--- src/main/webapp/js/GameModule.js
+++ src/main/webapp/js/GameModule.js
@@ -92,7 +92,7 @@ gameModule.controller('playerGamesController', ['$scope', '$http', '$location', 
 
 gameModule.controller('gameController', ['$rootScope', '$routeParams', '$scope', '$http',
     function (rootScope, routeParams, scope, http) {
-       var gameStatus;
+       var update_handler = setInterval(getInitialData, 600);
 
         function getInitialData() {
 
@@ -100,7 +100,10 @@ gameModule.controller('gameController', ['$rootScope', '$routeParams', '$scope',
                 //scope.gameProperties = data;
                 scope.gameProperties = data;
                 gameStatus = scope.gameProperties.gameStatus;
-                console.log(scope)
+                if(data.gameStatus == "F_PLAYER_WON" || data.gameStatus == "S_PLAYER_WON"){
+                    clearInterval(update_handler);
+                }
+                // console.log(scope)
                 getMoveHistory();
             }).error(function (data, status, headers, config) {
                 scope.errorMessage = "Failed do load game properties";
@@ -111,7 +114,6 @@ gameModule.controller('gameController', ['$rootScope', '$routeParams', '$scope',
             scope.gameProperties = data;
             console.log(scope)
         }
-        setInterval(getInitialData, 600);
 
         function getMoveHistory() {
             scope.movesInGame = [];
